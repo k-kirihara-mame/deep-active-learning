d278885decd5f1fcc4ec57ccd2aec1f751fc5b64
k-kirihara-mame <101682730+k-kirihara-mame@users.noreply.github.com>
2022-03-22 14:12:23 +0900

    Update nets.py

--- nets.py
+++ nets.py
@@ -158,3 +158,33 @@ class CIFAR10_Net(nn.Module):
 
     def get_embedding_dim(self):
         return 50
+
+class CIFAR10_VGG16_Net(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGGFeat, self).__init__()
+        cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+        self.features = self._make_layers(cfg)
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out_f = out.view(out.size(0), -1)
+        out = self.classifier(out_f)
+        return out, out_f
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)  
+    
+    def get_embedding_dim(self):
+        return 512
