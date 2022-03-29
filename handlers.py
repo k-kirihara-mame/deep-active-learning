6b79017bfcc8236dd737ae329bcc6da4674c8cfd
k-kirihara-mame <101682730+k-kirihara-mame@users.noreply.github.com>
2022-03-29 15:00:10 +0900

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -37,9 +37,8 @@ class CIFAR10_Handler(Dataset):
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
-        #self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
-        self.transform = transforms.Compose([transforms.RandomCrop(32, padding=4),transforms.RandomHorizontalFlip(),transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
-
+        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
+        
     def __getitem__(self, index):
         x, y = self.X[index], self.Y[index]
         x = Image.fromarray(x)
